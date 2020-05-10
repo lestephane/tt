4da853772216a1f6aadfe596f571fcbe414f414c
Le Stephane <contact@stephane.today>
2020-05-10 22:08:01 +0300

    add wbal wreg mbal mreg

--- tt.sh
+++ tt.sh
@@ -1,8 +1,40 @@
 #!/usr/bin/env bash
+#
+# punch clock tool based on hledger
+#
+# Environment:
+#   - TIME_FILE:
+#       Time entries file path. Date format placeholders are supported
+#       (example: $HOME/time/%Y-%m.timeclock)
+#
+#   - TIME_BUDGETS_FILE:
+#       Time budgets file path (example: $HOME/time/budgets.journal).
+#       When specified, balance reports include budget performance.
+#
+#     example budget file content:
+#     ----------------------------
+#     ~ weekly  personal budget
+#         wrk   40h
+#         fun   14h
+#     -----------------------------
+#
+#   - TIME_ALIASES_FILE:
+#       Optional time account aliases file path (example: $HOME/time/aliases.journal)
+#       When specified, time accounts get rewritten following the rules in this file.
+#       Due to the way hledger works, rules are processed from bottom to top.
+#       So, the modifications of later rules are seen by earlier rules.
+#
+#     example aliases file content:
+#     ----------------------------
+#     alias /^netflix/=fun:\0
+#     alias /^work(.*)/=wrk:\1
+#     ----------------------------
+#
 
 set -euo pipefail
 
 TIME_FILE=${TIME_FILE:-${HOME}/.hours.timeclock}
+TIME_FILE_FORMATTED=$(date +"${TIME_FILE}")
 
 function usage {
 	scriptName="$(basename ${0})"
@@ -14,6 +46,10 @@ i|in                "Time in" to begin a task. Args: [optional date] [task proje
 o|out               "Time out" to end a task. Args: [optional date]
 s|stat|t|tail       Show the end of the timeclock file. Args: [optional n lines]
 b|bal               Show daily balances.
+wb|wbal             Show weekly balances.
+wr|wreg             Show weekly register.
+mb|mbal             Show monthly balances.
+mr|mreg             Show monthly register.
 n|sw|next|switch    End (time out of) the current task, and begin the next.
                     task. New task name is the next arg.
 e|edit              Edit the timeclock file with \${EDITOR}.
@@ -35,19 +71,8 @@ function time_in {
 		time="$(date '+%Y-%m-%d') ${1}:00"
 		activity="${@:2}"
 	fi
-	#
-	# Split the activity into:
-	#
-	#   - the project: anything before the first whitespace(s)
-	#     whitespace-containing project name aren't supported.
-	#     (maps to the hledger transaction virtual account)
-	#
-	#   - the project task: anything following the first whitespace(s)
-	#     (maps to the hledger transaction description)
-	#
-	local project_task=$(echo "${activity}" | sed -E "s/[[:blank:]]+/  /")
-	echo "Begin ${project_task} at ${time}"
-	echo "i ${time} ${project_task}" >> $TIME_FILE
+	echo "Begin ${activity} at ${time}"
+	echo "i ${time} ${activity}" >> "${TIME_FILE_FORMATTED}"
 }
 
 function time_out {
@@ -56,7 +81,7 @@ function time_out {
 		time="$(date '+%Y-%m-%d') ${1}:00"
 	fi
 	echo "End at $time"
-	echo "o $time" >> $TIME_FILE
+	echo "o $time" >> "${TIME_FILE_FORMATTED}"
 }
 
 function tail_file {
@@ -64,7 +89,26 @@ function tail_file {
 	if [[ $# -gt 0 ]]; then
 		n=$1
 	fi
-	tail -n $n $TIME_FILE
+	tail -n $n "${TIME_FILE_FORMATTED}"
+}
+
+function extra_journals {
+  if [ -f "${TIME_BUDGETS_FILE}" ]; then
+    cat "${TIME_BUDGETS_FILE}"
+  fi
+  if [ -f "${TIME_ALIASES_FILE}" ]; then
+    cat "${TIME_ALIASES_FILE}"
+  fi
+}
+
+function time_file_pattern() {
+  echo "${TIME_FILE}" | sed 's/%[a-zA-Z]/*/g'
+}
+
+function time_file_include() {
+cat <<EOF
+!include $(time_file_pattern)
+EOF
 }
 
 case $1 in
@@ -81,23 +125,66 @@ case $1 in
 		tail_file $*
 		;;
 	b|bal)
-		hledger -f $TIME_FILE bal
+		hledger -f "${TIME_FILE_FORMATTED}" bal "$@"
+		;;
+	wb|wbal)
+		shift
+		hledger -f - -p "$(date -d "6 days ago" +%Y-%m-%d) to $(date -d "tomorrow" +%Y-%m-%d)" bal --budget not:"ignore|workflix|sleeping" "$@" <<-EOF | grep -vE ' 0 +$'
+		$(extra_journals)
+		$(time_file_include)
+		EOF
+		;;
+	wr|wreg)
+		shift
+		if head -n1 $(time_file_pattern) | grep -B1 -E "^o"; then
+			echo "time file does not start with a i entry"
+			false
+		fi
+		if tail -n1 $(time_file_pattern) | grep -B1 -E "^i"; then
+			echo "time file does not end with a o entry"
+			false
+		fi
+		hledger -f - -p "$(date -d "6 days ago" +%Y-%m-%d) to $(date -d "tomorrow" +%Y-%m-%d)" reg "$@" <<-EOF
+		$(time_file_include)
+		EOF
+		;;
+	mb|mbal)
+		shift
+		hledger -f - -p "$(date -d "last month" +%Y-%m-%d) to $(date -d "tomorrow" +%Y-%m-%d)" bal --budget not:"ignore|workflix|sleeping" "$@" <<-EOF | grep -vE ' 0 +$'
+		$(extra_journals)
+		$(time_file_include)
+		EOF
+		;;
+	mr|mreg)
+		shift
+		hledger -f - -p "$(date -d "last month" +%Y-%m-%d) to $(date -d "tomorrow" +%Y-%m-%d)" reg "$@" <<-EOF
+		$(time_file_include)
+		EOF
 		;;
 	d|daily)
-		hledger -f $TIME_FILE -p "daily this week" bal
+		hledger -f - -p "daily this week" bal --tree <<-EOF
+		$(extra_journals)
+		$(time_file_include)
+		EOF
 		;;
 	w|weekly)
-		hledger -f $TIME_FILE -p "weekly this month" bal
+		hledger -f - -p "weekly this month" bal --tree <<-EOF
+		$(extra_journals)
+		$(time_file_include)
+		EOF
 		;;
 	q|quarterly)
-		hledger -f $TIME_FILE -p "quarterly this year" bal
+		hledger -f - -p "quarterly this year" bal --tree <<-EOF
+		$(extra_journals)
+		$(time_file_include)
+		EOF
 		;;
 	n|sw|next|switch)
 		shift
 		time_out $* && time_in $*
 		;;
 	e|edit)
-		$EDITOR $TIME_FILE
+		$EDITOR "${TIME_FILE_FORMATTED}"
 		;;
 	*)
 		echo "Unknown command ${1}"
